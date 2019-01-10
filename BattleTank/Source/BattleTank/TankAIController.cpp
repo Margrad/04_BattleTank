deeeaf27bba203e31c2e748d4329f161502d6f8c
Margrad <marcogranada@gmail.com>
2019-01-10 13:07:37 +0000

    Lesson133 added AIControllers

---
+++ BattleTank/Source/BattleTank/TankAIController.cpp
@@ -0,0 +1,40 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TankAIController.h"
+
+void ATankAIController::BeginPlay() {
+	Super::BeginPlay();
+
+	AThank* ThisTank = GetControlledTank();
+	if (ThisTank) {
+		UE_LOG(LogTemp, Warning, TEXT("TankAIontroller is controlling: %s"), *ThisTank->GetName());
+	}
+	else {
+		UE_LOG(LogTemp, Error, TEXT("TankAIController is nullptr"));
+	}
+	if (GetPlayerTank()) {
+		UE_LOG(LogTemp, Warning, TEXT("%s is targeting: %s"), *ThisTank->GetName(), *PlayerTank->GetName());
+	}
+	else {
+		UE_LOG(LogTemp, Error, TEXT("TankAIController failed to identify Player"));
+	}
+}
+
+AThank* ATankAIController::GetControlledTank()
+{
+	return Cast<AThank>(GetPawn());
+}
+
+bool ATankAIController::GetPlayerTank()
+{
+	APlayerController* Player = GetWorld()->GetFirstPlayerController();
+	PlayerTank = Cast<AThank>(Player->GetPawn());
+	if (PlayerTank) {
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+
---
+++ BattleTank/Source/BattleTank/TankAIController.h
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Thank.h"
+#include "AIController.h"
+//#include "Engine.h"
+#include "TankAIController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BATTLETANK_API ATankAIController : public AAIController
+{
+	GENERATED_BODY()
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+public:
+	AThank* GetControlledTank();
+private:
+	bool GetPlayerTank();
+	AThank* PlayerTank = nullptr;
+};
